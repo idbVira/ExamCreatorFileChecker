6be6b2db8fef22dccdb278aac1547e3ac17002fa
RezaParsian <Rezaparsian76@gmail.com>
2024-08-24 14:12:43 +0330

    init

---
+++ README.md
@@ -0,0 +1,18 @@
+# ExamCreatorFileChecker
+
+------------------
+
+This project for check ExamCreator command and need:
+
+- [ ] pic folder
+- [ ] words folder
+
+-------------
+
+## Output
+| Error     | Mean                                                                              |
+|-----------|-----------------------------------------------------------------------------------|
+| Read: ... | reading {} file                                                                   |
+| File Not Exists: ... | {} not found in pic folder                                                        |
+| Wrong option: ... | {} has - in the line                                                              |
+| Wrong type: ... | type must be in: `space, join, sort, description, test, true, multiple, testsingle` |
---
+++ main.js
@@ -0,0 +1,117 @@
+const unzipper = require('unzipper');
+const fs = require('fs');
+const docs = fs.readdirSync('words');
+
+fs.readdirSync('pic').forEach(file => {
+    fs.renameSync('pic/' + file, 'pic/' + file.replace(/-/g, '_'));
+});
+
+function danger(msg) {
+    console.log(`\x1b[31m%s\x1b[0m`, msg);
+}
+
+function info(msg) {
+    console.log('\x1b[36m%s\x1b[0m', msg);
+}
+
+function extractTextFromWordXml(content) {
+    return content.replace(/<\/w:p>/g, '\n').replace(/<.*?>/g, '');
+}
+
+function checkFileExist(filePath) {
+    return fs.existsSync(filePath.replace('upload', 'pic'));
+}
+
+function checkImages(images) {
+    images.forEach(image => {
+        image = image.replace('+', '').trim();
+
+        if (image.includes('،')) {
+            const row = image.split('،');
+
+            row.forEach(item => {
+                if (item.trim().startsWith('upload'))
+                    if (!checkFileExist(item.trim()))
+                        danger(`File Not Exists: ${item}`)
+            });
+
+            return;
+        }
+
+        if (!checkFileExist(image.trim()))
+            danger(`File Not Exists: ${image}`)
+    })
+}
+
+function checkSyntax(content) {
+    const regex = /\*\*(?<type>.*?)\*\*(?<body>.*?)\n\*\*\/.*?\*\*/sg;
+
+    const types = {
+        'space': 'sort',
+        'join': 'join',
+        'sort': 'shuffle',
+        'description': 'descriptive',
+        'test': 'checkbox',
+        'true': 'trueFalse',
+        'multiple': 'checkboxMulti',
+        'testsingle': 'checkboxImg'
+    };
+
+    for (const match of content.matchAll(regex)) {
+        const type = match.groups.type.trim();
+
+        if (!types[type])
+            danger('Wrong type: ' + type)
+    }
+}
+
+function verifyFile(content) {
+    const images = content.match(/upload\/(?<image>.*)/mg);
+
+    if (!images)
+        return;
+
+    images.forEach(item => {
+        if (item.includes('-'))
+            danger(`Wrong option: ${item}`)
+    });
+
+    checkImages(images.filter(i => !i.includes('-')));
+    checkSyntax(content);
+}
+
+async function readZipFile(doc){
+    return new Promise(resolve => {
+        fs.createReadStream('./words/' + doc)
+            .pipe(unzipper.Parse())
+            .on('entry', (entry) => {
+                const fileName = entry.path;
+
+                if (fileName === 'word/document.xml') {
+                    let chunks = [];
+
+                    entry.on('data', (chunk) => {
+                        chunks.push(chunk);
+                    });
+
+                    entry.on('end', () => {
+                        verifyFile(extractTextFromWordXml(Buffer.concat(chunks).toString('utf-8')));
+                        resolve();
+                    });
+
+                } else {
+                    entry.autodrain();
+                }
+            })
+    });
+}
+
+(async function () {
+    for (const doc of docs){
+        info('Read: ' + './words/' + doc);
+        await readZipFile(doc)
+    }
+
+    info('\nPress enter to exit...');
+    process.stdin.read();
+})();
